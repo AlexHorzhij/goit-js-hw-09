a8603391a97470245902fe355c1f2223715495da
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2022-07-31 11:12:23 +0000

    Deploying to gh-pages from @ AlexHorzhij/goit-js-hw-09@15c4d443f24b86575b843a98c6843d6f4c401ad7 🚀

---
+++ 01-color-switcher.5ce2aeaf.js
@@ -0,0 +1,2 @@
+function t(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}({body:document.querySelector("body"),startBtn:document.querySelector([data-start]),stopBtn:document.querySelector([data-stop])}).startBtn.addEventListener("click",(function(){const o=t;body.styleList.backgroundColor=o}));
+//# sourceMappingURL=01-color-switcher.5ce2aeaf.js.map
---
+++ 01-color-switcher.e781df9f.js
@@ -0,0 +1,2 @@
+!function(){function t(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}({body:document.querySelector("body"),startBtn:document.querySelector([data-start]),stopBtn:document.querySelector([data-stop])}).startBtn.addEventListener("click",(function(){var o=t;body.styleList.backgroundColor=o}))}();
+//# sourceMappingURL=01-color-switcher.e781df9f.js.map
